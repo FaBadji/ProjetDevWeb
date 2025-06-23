801e0dca566dcf045d71a3163dc2ba500cc45881
FaBadji <badji.fatou@ugb.edu.sn>
2025-06-23 13:13:46 +0100

    23/06/2025

--- monScript.js
+++ monScript.js
@@ -1,5 +1,11 @@
-function ouvrirModal(imgSrc, titre, texte) {
-  document.getElementById("modalImg").src = imgSrc;
+// Variables globales pour le carrousel
+let imagesActuelles = [];
+let indexImage = 0;
+
+function ouvrirModal(images, titre, texte) {
+  imagesActuelles = images; // Tableau d'images
+  indexImage = 0;
+  document.getElementById("modalImg").src = imagesActuelles[indexImage];
   document.getElementById("modalTitre").innerHTML = titre;
   document.getElementById("modalTexte").innerHTML = texte;
   document.getElementById("modal").style.display = "block";
@@ -7,4 +13,22 @@ function ouvrirModal(imgSrc, titre, texte) {
 
 function fermerModal() {
   document.getElementById("modal").style.display = "none";
+}
+
+function suivantImage() {
+  if (indexImage < imagesActuelles.length - 1) {
+    indexImage++;
+    document.getElementById("modalImg").src = imagesActuelles[indexImage];
+  }
+}
+
+function precedentImage() {
+  if (indexImage > 0) {
+    indexImage--;
+    document.getElementById("modalImg").src = imagesActuelles[indexImage];
+  }
+}
+
+function validerChoix() {
+  alert("✅ Vous avez choisi ce logement !");
 }
